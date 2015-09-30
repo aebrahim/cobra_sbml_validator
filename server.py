fde01585091dfdcfeb1a29386436c1ba1bc22e50
Ali Ebrahim <aebrahim@ucsd.edu>
2015-09-30 13:52:11 -0700

    add ability to validate JSON

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ import tornado
 import tornado.ioloop
 import tornado.web
 
+import jsonschema
+
 import cobra
 
 
@@ -52,24 +54,49 @@ class Upload(tornado.web.RequestHandler):
         else:
             contents = StringIO(body)
 
-        try:
-            model, sbml_errors = cobra.io.sbml3.validate_sbml_model(contents)
-        except cobra.io.sbml3.CobraSBMLError as e:
-            self.send_error(415, reason=e.message)
-            return
+        # syntax validation
+        # if the model can't be loaded from the file it's considered invalid
+        if filename.endswith(".json") or filename.endswith(".json.gz") or \
+                filename.endswith(".json.bz2"):
+            try:
+                model_json = cobra.io.json.json.load(contents)
+            except ValueError as e:
+                self.send_error(415, reason="Invalid JSON:" + e.message)
+                return
+            try:
+                model = cobra.io.json._from_dict(model_json)
+            except Exception as e:
+                self.send_error(415, reason="Invalid model:" + e.message)
+                return
+            try:
+                jsonschema.validate(model_json, cobra.io.json.json_schema)
+            except jsonschema.ValidationError as e:
+                errors = [e.message]
+            else:
+                errors = []
 
+        else:  # SBML validation
+            try:  # this function fails if a model can not be created
+                model, errors = cobra.io.sbml3.validate_sbml_model(contents)
+            except cobra.io.sbml3.CobraSBMLError as e:
+                self.send_error(415, reason=e.message)
+                return
+
+        # model validation
+        warnings = []
         solution = model.optimize(solver="esolver")
         if solution.status != "optimal":
-            sbml_errors.append("model can not be solved (status '%s')" %
-                               solution.satus)
+            errors.append("model can not be solved (status '%s')" %
+                          solution.satus)
         elif solution.f <= 0:
-            sbml_errors.append("model can not produce nonzero biomass")
+            warnings.append("model can not produce nonzero biomass")
         elif solution.f <= 1e-3:
-            sbml_errors.append("biomass flux %s too low" % str(solution.f))
+            warnings.append("biomass flux %s too low" % str(solution.f))
 
-        response = {"sbml_errors": sbml_errors}
-        response["solution_status"] = solution.status
-        response["objective"] = str(solution.f)
+        response = {"errors": errors,
+                    "warnings": warnings,
+                    "solution_status": solution.status,
+                    "objective": str(solution.f)}
         self.finish(dumps(response))
 
 
