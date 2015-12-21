585d4cc319395270b860a9c0453ec2cad7b64603
Ali Ebrahim <aebrahim@ucsd.edu>
2015-12-21 11:29:11 -0800

    restructure into single file and add MIT license

--- cobra_sbml_validator.py
+++ cobra_sbml_validator.py
@@ -13,14 +13,15 @@ import tornado.web
 import tornado.gen
 import tornado.concurrent
 
-from six import BytesIO, StringIO
+from six import BytesIO, StringIO, iteritems
 import jsonschema
 
 import cobra
+from cobra.core.Gene import parse_gpr
+from cobra.manipulation import check_mass_balance, check_reaction_bounds, \
+    check_metabolite_compartment_formula
+
 from libsbml import SBMLValidator
-
-from validator import validate_model
-
 
 executor = tornado.concurrent.futures.ThreadPoolExecutor(8)
 
@@ -105,6 +106,53 @@ def decompress_file(body, filename):
     else:
         contents = BytesIO((body))
     return contents, None
+
+
+def validate_model(model):
+    errors = []
+    warnings = []
+    errors.extend(check_reaction_bounds(model))
+    errors.extend(check_metabolite_compartment_formula(model))
+    # test gpr
+    for reaction in model.reactions:
+        try:
+            parse_gpr(reaction.gene_reaction_rule)
+        except SyntaxError:
+            errors.append("reaction '%s' has invalid gpr '%s'" %
+                          (reaction.id, reaction.gene_reaction_rule))
+    # test mass balance
+    for reaction, balance in iteritems(check_mass_balance(model)):
+        # check if it's a demand or exchange reaction
+        if len(reaction.metabolites) == 1:
+            warnings.append("reaction '%s' is not balanced. Should it "
+                            "be annotated as a demand or exchange "
+                            "reaction?" % reaction.id)
+        elif "biomass" in reaction.id.lower():
+            warnings.append("reaction '%s' is not balanced. Should it "
+                            "be annotated as a biomass reaction?" %
+                            reaction.id)
+        else:
+            warnings.append("reaction '%s' is not balanced for %s" %
+                            (reaction.id, ", ".join(sorted(balance))))
+
+    # try solving
+    solution = model.optimize(solver="esolver")
+    if solution.status != "optimal":
+        errors.append("model can not be solved (status '%s')" %
+                      solution.status)
+        return {"errors": errors, "warnings": warnings}
+
+    # if there is no objective, then we know why the objective was low
+    if len(model.objective) == 0:
+        warnings.append("model has no objective function")
+    elif solution.f <= 0:
+        warnings.append("model can not produce nonzero biomass")
+    elif solution.f <= 1e-3:
+        warnings.append("biomass flux %s too low" % str(solution.f))
+    if len(model.objective) > 1:
+        warnings.append("model should only have one reaction as the objective")
+
+    return {"errors": errors, "warnings": warnings, "objective": solution.f}
 
 
 class Upload(tornado.web.RequestHandler):
